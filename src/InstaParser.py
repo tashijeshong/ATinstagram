1da00b0d7e6feb47a1ea17685619eb8c7a9385a6
Nicholas Halstead <73657669+nhalsteadvt@users.noreply.github.com>
2022-01-24 16:53:22 -0500

    Cleaned up code a bit
    all_tags and popular_tags now take in JSON formatted strings instead of files

--- src/InstaParser.py
+++ src/InstaParser.py
@@ -1,28 +1,23 @@
 import json
 
-BASE = "../data/"
+BASE = "../data/" ###CHANGE THIS DEPENDING ON WHERE YOU'RE RUNNING THE PROJECT
 
 # This is the main function that runs the program
 def main():
-    tag1 = all_tags(BASE + "res1.txt", 0.1) #extracts all tags from a file
-    # print(tag1)
-    tag2 = all_tags(BASE + "res2.txt", 0.1)
-    # print(tag2)
-    tag3 = all_tags(BASE + "res3.txt", 0.1)
-    # print(tag3)
+    file1 = BASE + "res1.txt"
+    file2 = BASE + "res2.txt"
+    file3 = BASE + "res3.txt"
+    html1 = open(file1, 'r', encoding="utf8").read()
+    html2 = open(file2, 'r', encoding="utf8").read()
+    html3 = open(file3, 'r', encoding="utf8").read()
+
+    tag1 = all_tags(html1) #extracts all tags from a file
+    tag2 = all_tags(html2)
+    tag3 = all_tags(html3)
 
     allTags = combine_tags(tag1, tag2) #combines two results into one (assumes that their contents are mutually exclusive, will program a check in later)
     allTags = combine_tags(allTags, tag3)
     print(top_tags(allTags, 0.25)) #prints the top tags of combined results (top 25%)
-
-
-    # file1 = BASE + "res1.txt"
-    # html_doc1 = open(file1, 'r', encoding="utf8").read()
-    # json1 = read_json(html_doc1)
-    # print("Number of entries: ", len(json1['data']['hashtag']['edge_hashtag_to_media']['edges']))
-    # print("Has next page: ", json1['data']['hashtag']['edge_hashtag_to_media']['page_info']['has_next_page'])
-    # print("End cursor: ", json1['data']['hashtag']['edge_hashtag_to_media']['page_info']['end_cursor'])
-
 
 
 
@@ -32,50 +27,43 @@ def main():
 # The format of the desired output is as follows: ..."text":"<VALUE>"...
 def parse_json(html):
     ans = []
-    for i in range(len(html)):
-        if(html[i] == '"'):
-            if(html[i+1] == 't' and html[i+2] == 'e' and html[i+3] == 'x' and html[i+4] == 't' and html[i+5] == '"'):
-                i = i + 8
-                while(html[i] != '"'):
-                    ans.append(html[i])
-                    i = i + 1
-    string = ""
-    for a in ans:
-        string += a
-    return string
+    json = read_json(html)
+    for i in range(len(json['data']['hashtag']['edge_hashtag_to_media']['edges'])):
+        edge = json['data']['hashtag']['edge_hashtag_to_media']['edges'][i]['node']['edge_media_to_caption']['edges']
+        if len(edge) > 0:
+            ans.append(edge[0]['node']['text'])
+        else:
+            ans.append("")
+
+    return ans
+
 
 # This function parses text for tags, specifically for "#<TAG>"
-def parse_tags(text):
-    ans = []
-    for i in range(len(text)):
-        if(text[i] == '#'):
-            ans.append('#')
-            i = i + 1
-            if i >= len(text):
-                print("break")
-                break
-            while(text[i] != ' ' and text[i] != '\\'):
-                ans.append(text[i])
-                i = i + 1
-                if i >= len(text):
-                    print("break")
-                    ans.append(' ')
-                    break
-            ans.append(' ')
-    string = ""
-    for a in ans:
-        string += a
-    return string
-
-# This function cleans up empty tags from a list of tags
-# Removes any tag that has a space right after it
-def clean_tags(tags):
-    ans = tags.split('#')
+def parse_tags(descriptions):
     cleanTags = []
-    for i in range(len(ans)):
-        if len(ans[i]) > 1:
-            cleanTags.append(ans[i])
+    for desc in descriptions:
+        tmpTag = ""
+        # add every tag to tmpTag
+        for i in range(len(desc)):
+            if desc[i] == '#':
+                tmpTag += '#'
+                i = i + 1
+                if i >= len(desc):
+                    break
+                while(desc[i] != ' ' and desc[i] != '#' and desc[i] != '\n' and desc[i] != '\r'):
+                    tmpTag += desc[i]
+                    i = i + 1
+                    if i >= len(desc):
+                        break
+                tmpTag += ' '
+        tags = tmpTag.split(' ')
+        
+        for tag in tags:
+            if len(tag) > 1:
+                cleanTags.append(tag)
+
     return cleanTags
+
 
 # This function takes an array of cleaned tags and returns dictionary of each tag and its frequency
 def tag_freq(tags):
@@ -100,33 +88,20 @@ def top_tags(tags, tagFreq=0.1):
     return topTags
 
 
-# This function takes a file and returns a dictionary of the most popular tags in the file
-def popular_tags(file, tagFreq):
-    print("Starting...")
-    #set html_doc to the contents of the entire file
-    html_doc = open(file, 'r', encoding="utf8").read()
-    print("Parsing...")
-    parsedJSON = parse_json(html_doc)
-    parsedTags = parse_tags(parsedJSON)
-    cleanTags = clean_tags(parsedTags)
-    cleanTags.sort()
-    freqTags = tag_freq(cleanTags)
-    print("Done!")
-    return top_tags(freqTags, tagFreq)
+# This function takes a JSON formatted string and returns a dictionary of the most popular tags
+def popular_tags(html, tagFreq):
+    parsedJSON = parse_json(html)
+    tagList = parse_tags(parsedJSON)
+    tagDict = tag_freq(tagList)
+    topTags = top_tags(tagDict, tagFreq)
+    return topTags
 
-# This function takes a file and returns a dictionary of all tags in the file
-def all_tags(file, tagFreq):
-    print("Starting...")
-    #set html_doc to the contents of the entire file
-    html_doc = open(file, 'r', encoding="utf8").read()
-    print("Parsing...")
-    parsedJSON = parse_json(html_doc)
-    parsedTags = parse_tags(parsedJSON)
-    cleanTags = clean_tags(parsedTags)
-    cleanTags.sort()
-    freqTags = tag_freq(cleanTags)
-    print("Done!")
-    return freqTags
+# This function takes a JSON formatted string and returns a dictionary of all tags
+def all_tags(html):
+    parsedJSON = parse_json(html)
+    tagList = parse_tags(parsedJSON)
+    tagDict = tag_freq(tagList)
+    return tagDict
 
 # This function takes two dictionary of tags and their count and returns a dictionary of the two inputs combined, counts are added together
 def combine_tags(tags1, tags2):
